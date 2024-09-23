83c6024746ac8665b4a54c40d04bd6b3f87021c1
Sry Deviani Tambunan <89495018+srydevianitambunan@users.noreply.github.com>
2024-09-23 11:51:50 +0700

    changes folder

---
+++ Github/Vocabee/Vocabee/VocabeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabeeApp.swift
+//  Vocabee
+//
+//  Created by Sry Tambunan on 23/09/24.
+//
+
+import SwiftUI
+
+@main
+struct VocabeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Github/Vocabee/VocabeeTests/VocabeeTests.swift
@@ -0,0 +1,17 @@
+//
+//  VocabeeTests.swift
+//  VocabeeTests
+//
+//  Created by Sry Tambunan on 23/09/24.
+//
+
+import Testing
+@testable import Vocabee
+
+struct VocabeeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
